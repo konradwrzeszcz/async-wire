34ec28b1b660be690c2eb745fd00e0225b6fcf9e
konradwrzeszcz <konrad.wrzeszcz@deliveroo.com>
2023-09-07 19:33:49 +0200

    removed main2

--- main.go
+++ main.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"fmt"
-	"github.com/stephennancekivell/go-future/future"
-	"time"
 )
 
 func main() {
@@ -20,21 +18,4 @@ func main() {
 	fmt.Println("menu icon url", menu.Icon.URL)
 	fmt.Println("menu brand name", menu.Brand.Name)
 	fmt.Println("menu brand image url", menu.Brand.Img.URL)
-}
-
-func main2() {
-	f := future.New(func() string {
-		fmt.Println("before sleep")
-		time.Sleep(2 * time.Second)
-		fmt.Println("after sleep")
-		return "value"
-	})
-
-	fmt.Println("before get")
-	value := f.Get()
-	fmt.Println("after get")
-	fmt.Println(value)
-
-	value2 := f.Get()
-	fmt.Println(value2)
 }
